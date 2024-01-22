c0e96eb37cdd5762044262ba1bb867cecad2ba68
Alexander Bartolomey <occloxium@gmail.com>
2024-01-22 01:11:03 +0100

    docs: output descriptions
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,14 @@
 output "nodes_ipv4" {
   value = local.node_ipv4_addresses
+  description = "Node IPv4 addresses that were created"
 }
 
 output "nodes_ipv6" {
   value = local.node_ipv6_addresses
+  description = "Node IPv6 addresses that were created"
 }
-
-# output "nodes_ipv4_lb_cidrs" {
-#   value = local.node_ipv4_lb_cidrs
-# }
-
-# output "nodes_ipv6_lb_cidrs" {
-#   value = local.node_ipv6_lb_cidrs
-# }
-
-# output "ca_cert_hash" {
-#   value = local.ca_cert_hash
-# }
 
 output "kubeconfig_yaml" {
   value = module.kubeconfig.yaml
+  description = "The (super)admin kubeconfig created on the leader of the control-plane. You might want to replace this with a less permissive kubeconfig afterwards..."
 }
-
-# output "kubeconfig_hcl" {
-#   value = module.kubeconfig.hcl
-# } 
